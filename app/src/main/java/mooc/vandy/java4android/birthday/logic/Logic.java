43328315ce37a05a2715ce963d0697af50d0a137
Anuj <dagadeanuj@gmail.com>
2022-10-13 21:17:23 +0530

    Update Logic.java

--- app/src/main/java/mooc/vandy/java4android/birthday/logic/Logic.java
+++ app/src/main/java/mooc/vandy/java4android/birthday/logic/Logic.java
@@ -74,7 +74,40 @@ public class Logic
      */
     public double calculate(int size, int count) {
         // TODO -- add your code here
+        
+       double result = 0.0;
 
+      for(int i=1; i<=count; i++) {
+
+        Map<Integer,Integer> birthMap = new HashMap<Integer,Integer>();
+        Random day = new Random();
+        day.setSeed(i);
+
+          for(int j=1; j<=size; j++) {
+
+            int ranVal =  day.nextInt(365);
+
+            if(!birthMap.containsKey(ranVal)) {
+              birthMap.put(ranVal, 1);
+            }else {
+
+                if(birthMap.get(ranVal)>=2)
+                {
+                    break;
+                }
+              birthMap.put(ranVal, birthMap.get(ranVal)+1);
+            }
+            
+          }
+
+          if(birthMap.containsValue(2)) {
+            result++;
+          }
+
+
+        }
+        System.out.println(result);
+        return result*100/count;
         
     }
 
